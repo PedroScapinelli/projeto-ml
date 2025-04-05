adb32a0581979dc5a92a8bd9f7045fed0edfe699
Pedro <phenriquelscapinelli@gmail.com>
2025-04-05 16:56:10 -0300

    Teste

--- tests/app.py
+++ tests/app.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import streamlit as st
+from sklearn.linear_model import linearRegression
+
+df =pd.read_csv("pizzas.csv")
+
+modelo = linearRegression()
+x = df[["diametro"]]
+y = df[["preco"]]
+
+st.title("Calculadora de preço de pizzas")
